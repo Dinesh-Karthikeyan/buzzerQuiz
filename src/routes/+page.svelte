bfe83d8a80351593f8ca62b72e0ca357cbab1476
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-07-29 22:28:18 +0530

    created landing page with login and register option

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<main>
+    <a href="/login">
+        <button>Log IN</button>
+    </a>
+    <a href="/register">
+        <button>Sign UP</button>
+    </a>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 4rem;
+        height: 100vh;
+        width: 100vw;
+    }
+    button {
+        padding: 1rem;
+        font-size: 1.5rem;
+        border: 2px solid black;
+        background-color: white;
+        border-radius: 1rem;
+        transition-duration: 250ms;
+    }
+    button:hover {
+        background-color: black;
+        color: white;
+    }
+</style>
