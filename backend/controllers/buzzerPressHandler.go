51d141d56ab4c2d7e28d0a36a7f44099265e6de1
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-07-31 00:46:32 +0530

    restructured buzzerPressHandler

--- backend/controllers/buzzerPressHandler.go
+++ backend/controllers/buzzerPressHandler.go
@@ -10,27 +10,6 @@ var question models.Question
 var buzzerQueue = make(chan *fiber.Ctx, 10)
 var counter int = 0
 
-// to present the first three users the question
-func processRequest(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"question": question.Question,
-		"id":       question.ID,
-		"A":        question.A,
-		"B":        question.B,
-		"C":        question.C,
-		"D":        question.D,
-	})
-}
-
-// to get the first three users
-func processBuzzerPress(queue chan *fiber.Ctx, count *int) {
-	if *count < 3 {
-		c := <-queue
-		processRequest(c)
-		*count = *count + 1
-	}
-}
-
 // to process and queue the buzzer request
 func buzzerPressHandler(c *fiber.Ctx) error {
 	// Add the request to the buzzerQueue
