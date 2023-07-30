e61514c97098161971870e0dfc0c2d29ecd0950e
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-07-30 20:15:49 +0530

    handling login for new devices

--- backend/controllers/login.go
+++ backend/controllers/login.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"backend/database"
 	"backend/models"
+	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -31,8 +34,45 @@ func Login(c *fiber.Ctx) error {
 			"message": "Incorrect Password",
 		})
 	}
+	//checking if this is the first login
+	if user.Jwt == "" {
+		claims := &jwt.StandardClaims{
+			Issuer:    strconv.Itoa(int(user.ID)),
+			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+		}
 
-	var jwtToken = c.Cookies("jwt")
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		ss, err := token.SignedString(secret)
+
+		if err != nil {
+			c.Status(fiber.StatusInternalServerError)
+			return c.JSON(fiber.Map{
+				"message": "Could not sign token during registration with secret",
+			})
+		}
+
+		cookie := fiber.Cookie{
+			Name:     "jwt",
+			Value:    ss,
+			Expires:  time.Now().Add(time.Hour * 24),
+			HTTPOnly: true,
+		}
+
+		c.Cookie(&cookie) // prepared and sent a cookie to the newly registered user
+
+		user.Jwt = ss
+		database.DB.Save(&user) // updated the user's jwt field in the database
+	}
+
+	err = database.DB.Where("team_name=?", data["team_name"]).First(&user).Error
+	if err != nil {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+	var jwtToken = c.Cookies("jwt") // checking for dual login
 	if jwtToken != user.Jwt && jwtToken != "" {
 		c.Status(fiber.StatusUnauthorized)
 		return c.JSON(fiber.Map{
